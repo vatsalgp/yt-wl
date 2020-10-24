1440afac6cfed4361ab4faa1e79a052b57c0b151
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-10-24 18:05:37 +0530

    Add files via upload

---
+++ public.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+    $("ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(2) > div:nth-child(3) > ytd-menu-renderer:nth-child(1) > yt-icon-button:nth-child(2) > button:nth-child(1) > yt-icon:nth-child(1)").click();
+    $("ytd-menu-service-item-renderer.style-scope:nth-child(3) > paper-item:nth-child(1)").click();
+}, 100);
