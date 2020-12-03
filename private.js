35eef21d326aa7988f1b63061e577bb832faf2de
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-12-04 01:12:23 +0530

    Update private.js

--- private.js
+++ private.js
@@ -1,4 +1,6 @@
-setInterval(() => {
+const func = $ => {
     $("ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(2) > div:nth-child(3) > ytd-menu-renderer:nth-child(1) > yt-icon-button:nth-child(2) > button:nth-child(1) > yt-icon:nth-child(1)").click();
     $("ytd-menu-service-item-renderer.style-scope:nth-child(1) > paper-item:nth-child(1)").click();
-}, 100);
+};
+
+setInterval(func, 100, $);
